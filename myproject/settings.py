09925ed76d1f352a4265fa13b283a38a1655501d
paula <p.rodriguezmartin@alumnos.urjc.es>
2021-06-01 22:15:09 +0200

    python anyhere 2

--- myproject/settings.py
+++ myproject/settings.py
@@ -123,9 +123,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
-#STATIC_URL = '/static/'
-STATIC_URL = 'myproject/templates/'
+STATIC_ROOT = os.path.join(BASE_DIR, "templates")
+#STATIC_URL = '/static/'STATIC_URL = 'myproject/templates/'
 STATICFILES_FINDERS = (
     "django.contrib.staticfiles.finders.FileSystemFinder",
     "django.contrib.staticfiles.finders.AppDirectoriesFinder",
