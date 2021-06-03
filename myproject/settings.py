d074c6e3f6f6c0a619c13792618ba121217d0fad
paula <p.rodriguezmartin@alumnos.urjc.es>
2021-06-03 02:35:38 +0200

    añadido imagenes logo con niveles

--- myproject/settings.py
+++ myproject/settings.py
@@ -125,7 +125,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 #STATIC_ROOT = os.path.join(BASE_DIR, "templates")
-STATIC_URL = '/static/'
+STATIC_URL = 'templates/'
 #
 # STATIC_ROOT = "/home/prodriguezmartin/snap/templates"
 
